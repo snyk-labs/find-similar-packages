5fe55780b4cffa6cdfea4061a0658bc011c920f5
Chris Huszcza <chris.huszcza@snyk.io>
2021-02-10 11:17:22 +0000

    Small changes to the script to make it runnable

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fetchKeys = require('fetch-package-keywords');
 const top = require('top-packages-category-wise');
+const RegClient = require('silent-npm-registry-client');
 
 const topSelectSize = 100; // how many packages to fetch for given keyword/category
-const defaultResultSize = 10;
+const defaultResultSize = 3;
 
 function sortToArray(map) {
     var list = [], map2 = {}, maxIndex = 0;
@@ -11,7 +12,8 @@ function sortToArray(map) {
         var pa = map2[score];
 
         if (!pa)
-            pa = [], map2[score] = pa;
+            pa = [];
+            map2[score] = pa;
 
         pa.push(package);
 
@@ -26,43 +28,120 @@ function sortToArray(map) {
             Array.prototype.push.apply(list, pa);
     }
 
+
     return list;
 }
 
-module.exports = (package, size) => {
+function similarEnoughToSkip(inputPkgDetails, candidatePkgDetails) {
+    // first we check if its smth like 'yorkie' and '@anejs/yorkie', in which case
+    // its highly likely they are just forks
+    let pkg1Name = inputPkgDetails.name;
+    let pkg2Name = candidatePkgDetails.name;
+    if (pkg1Name.includes('/')) {
+        pkg1Name = pkg1Name.split("/")[1]
+    }
+    if (pkg2Name.includes('/')) {
+        pkg2Name = pkg2Name.split("/")[1]
+    }
+    if (pkg1Name === pkg2Name) {
+        return true;
+    }
+
+    // secondly we check if they come from the same repository name, in which case we will also skip (likely forks)
+    if ('repository' in inputPkgDetails && 'repository' in candidatePkgDetails.links) {
+        let inputPkgUrl = inputPkgDetails.repository.url
+            .replace("git+", "")
+            .replace(".git", "");
+
+        let inputPkgRepoName = inputPkgUrl.split("/").pop();
+
+        let candidatePkgUrl = candidatePkgDetails.links.repository;
+        let candidatePkgRepoName = candidatePkgUrl.split("/").pop();
+
+        if (inputPkgRepoName === candidatePkgRepoName) {
+            return true;
+        }
+    }
+    // if repository not available, use homepage to instead
+    else if ('homepage' in inputPkgDetails && 'homepage' in candidatePkgDetails.links) {
+        if (inputPkgDetails.homepage === candidatePkgDetails.links.homepage) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+function fetchPkgDetails(pkgName) {
+    let options = { timeout: 1000 };
+    const client = new RegClient();
     return new Promise((resolve, reject) => {
-        if (!size)
+        client.get('https://registry.npmjs.org/' + packageName, options, function (error, data, raw, res) {
+            if (error)
+                reject(error);
+
+            resolve(JSON.parse(raw));
+        });
+    });
+}
+
+
+function findSimilar(inputPkgName, size) {
+    return new Promise((resolve, reject) => {
+        if (!size) {
             size = defaultResultSize;
+        }
 
         var similar = new Map();
 
-        fetchKeys(package).then(function (keys) {
-            if (!keys || keys.length == 0)
+        fetchPkgDetails(inputPkgName).then(function (inputPkgDetails) {
+            let keys = (inputPkgDetails.keywords ? inputPkgDetails.keywords : []);
+            if (!keys || keys.length === 0) {
                 resolve([]);
+            }
 
             // got keywords for package
             var remainKeys = keys.length;
 
             keys.forEach(function (key) {
 
-                top(key, topSelectSize).then(function (pkgs) {
+                let options = {
+                    fullPackageDetail: true
+                };
 
-                    pkgs.forEach(function (p) {
-                        if (p === package)
+                top(key, topSelectSize, options).then(function (pkgs) {
+
+                    pkgs.forEach(function (candidatePkgDetails) {
+                        if (candidatePkgDetails.name === inputPkgName) {
                             return;
+                        }
 
-                        var existing = similar.get(p);
-                        similar.set(p, existing ? ++existing : 1);
+                        if (similarEnoughToSkip(inputPkgDetails, candidatePkgDetails)) {
+                            return;
+                        }
+
+                        var existing = similar.get(candidatePkgDetails.name);
+                        similar.set(candidatePkgDetails.name, existing ? ++existing : 1);
                     });
 
-                    if (--remainKeys == 0)
-                        resolve(sortToArray(similar).slice(0, size));
-
+                    if (--remainKeys === 0) {
+                        let output = sortToArray(similar);
+                        output = output.slice(0, size);
+                        resolve(output);
+                    }
                 });
             });
         }).catch((error) => { reject(error); });
 
     });
-};
+}
 
+let inputArgs = process.argv.slice(2);
+let packageName = inputArgs[0];
+findSimilar(packageName).then(function (packages) {
+    console.log(packages);
+}).catch(function (error) {
+    console.log(error);
+});
 
